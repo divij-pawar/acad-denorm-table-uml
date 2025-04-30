d7519c745b4f68eebf8a754c3978384c6889443d
divij-pawar <divijpawar9@gmail.com>
2025-04-30 09:44:34 -0400

    added data transform code

---
+++ transform.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_excel("summit-download-1.xlsx")
+dissertation = pd.read_excel("Disserations_2022.xlsx")
+all_grad_students = pd.read_excel("01_All_Graduating_Students.xlsx")
+hood_colors = pd.read_excel("Hood_Colors.xlsx")
+
